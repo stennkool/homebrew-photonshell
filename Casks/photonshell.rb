c26eafe995f6cef854b9b4a2a9911d99eac1747a
GitHub Action <action@github.com>
2026-02-20 08:30:36 +0000

    Update PhotonShell to 0.0.1-a20

--- Casks/photonshell.rb
+++ Casks/photonshell.rb
@@ -1,6 +1,6 @@
 cask "photonshell" do
-  version "0.0.1-a19"
-  sha256 "e2efca793ccc25b4064985b8f454d4c2dda6a35e5f8698f3841bb8d8a29115ef"
+  version "0.0.1-a20"
+  sha256 "058a9ec01e311a60de177569567ab67d2fb90082e18ac88ba872186cc3900729"
 
   url "https://www.photonshell.com/releases/PhotonShell-#{version}-macos-arm64.dmg"
   name "PhotonShell"
